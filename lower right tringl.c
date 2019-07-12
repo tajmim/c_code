e91b5e52284ebf889a12b83caa7852ceec82fc05
tanha <52608165+tajmim@users.noreply.github.com>
2019-07-11 23:58:54 -0700

    adding comment

--- lower right tringl.c
+++ lower right tringl.c
@@ -5,6 +5,7 @@ main()
 	int i,j,k=3,sum=0,x[4][4];
 	//clrscr();
 	for(i=0;i<=3;i++)
+		
 	{
 		printf("Enter numbers for row %d:\n",i+1);
 		for(j=0;j<=3;j++)
@@ -24,7 +25,7 @@ main()
 		sum=sum+x[i][j];
 		k--;
 	}
-
+        /* this is comment */
 	printf("\nSummation of lower right triangle=%d\n",sum);
 	getch();
 	return 0;
